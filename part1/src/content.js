5e5432fe0bd5e39f408a2008f8c77b45266e1da2
MuraliAchanti <muralivenkata4@gmail.com>
2020-04-19 20:10:12 +0530

    1.3

--- part1/src/content.js
+++ part1/src/content.js
@@ -3,9 +3,9 @@ import Part from './part';
 
 const Content = (props) => (
     <>
-        <Part part = {props.part1} exercises = {props.exercises1}/>
-        <Part part = {props.part2} exercises = {props.exercises2}/>
-        <Part part = {props.part3} exercises = {props.exercises3}/>
+        <Part part = {props.part1}/>
+        <Part part = {props.part2}/>
+        <Part part = {props.part3}/>
     </>
 )
 
